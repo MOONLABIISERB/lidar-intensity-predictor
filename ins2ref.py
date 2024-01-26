522c6a01cffe6aec57e6e67b76a187784e2980c7
Kasi-Viswanath <62610515+kasiv008@users.noreply.github.com>
2024-01-26 14:18:11 -0600

    Update ins2ref.py

--- ins2ref.py
+++ ins2ref.py
@@ -1,12 +1,6 @@
-#import open3d
+
 import numpy as np
-
-#import numpy as np
-#from plyreader import PlyReader
-#import matplotlib.pyplot as plt
 import os
-from multiprocessing.dummy import Pool
-
 import alpha_predictor.alpha_model as alpha_model
 import torch
 import torch.backends.cudnn as cudnn
@@ -15,12 +9,10 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 cudnn.benchmak = False
 cudnn.deterministic = True
 cudnn.enabled = True
-torch.cuda.set_device(0)
+torch.cuda.set_device(device)
 
-#root = '/media/moonlab/sd_card/Rellis_3D_lidar_example/'
 models = alpha_model.alpha()
 models.to(device)
-#print(models)
 models.load_state_dict(torch.load('./alpha_predictor/models/best_model_mega_tanh.pth')['model_state_dict'])
 
 def load_bin(bin_path):
@@ -38,36 +30,27 @@ def alpha_predictor(input):
     return angles
 
 def ref_conv(points):
-    #pr = PlyReader()
-    #plydata = pr.open(ply_path)
-    #vertex =plydata['vertex']
-    #print('lol')
-    #points = load_bin('/home/usl/Desktop/intensity_salsa/Rellis_3D_os1_cloud_node_kitti_bin/Rellis-3D/00003/os1_cloud_node_kitti_bin/'+bin_path)
-    #labels = np.fromfile('/home/usl/Desktop/intensity_salsa/Rellis_3D_os1_cloud_node_semantickitti_label_id_20210614/Rellis-3D/00003/os1_cloud_node_semantickitti_label_id/'+bin_path[:-3]+'label',dtype = np.int32,count = -1)
-    #pc = open3d.geometry.PointCloud()
     x,y,z,ins= points[:,0],points[:,1],points[:,2],(points[:,3]*65535)
-    rang = np.sqrt(x**2+y**2+z**2)
+    range = np.sqrt(x**2+y**2+z**2)
     dis = x**2 + y**2 + z**2
 
     #removing lidar points less than 1 meter range(near range effect) 
-    ind = np.where(rang < 1)[0]
+    ind = np.where(range < 1)[0]
     ins = np.delete(ins,list(ind))
     x = np.delete(x,list(ind))
     y = np.delete(y,list(ind))
     z = np.delete(z,list(ind))
-    rang = np.delete(rang,list(ind))
+    range = np.delete(rang,list(ind))
     dis = np.delete(dis,list(ind))
-    #labels = np.delete(labels,list(ind))
 
 
     points_n = np.zeros((len(x),3))
     points_n[:,0],points_n[:,1],points_n[:,2] = x , y, z
     ld_vec = points_n/np.sqrt(dis[:,np.newaxis])
-    #pc.points = open3d.utility.Vector3dVector(points)
-    #pc.estimate_normals(search_param = open3d.geometry.KDTreeSearchParamHybrid(radius = 0.5,max_nn = 10))
-    #open3d.visualization.draw_geometries([pc])
-    #nm = np.asarray(pc.normals)
+    
+    #predicting angle of incidence.
     angles = alpha_predictor(ld_vec)
+    #claibrating intensity for reflectivity
     cal_intensity = ins*dis/np.cos(angles)/6553500
 
     points_n = np.zeros((len(x),4),dtype = np.float32)
